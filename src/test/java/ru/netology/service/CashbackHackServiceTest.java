c220ea5d9cf6723093e1707cc4de68628849b63d
OLEG PHILIPPOV <5157175@bk.ru>
2022-07-30 13:41:48 +0300

    added tests in junit4 branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
@@ -10,33 +10,33 @@ public class CashbackHackServiceTest {
     public void testRemainAmount900() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
-        assertEquals(actual, 100);
+        assertEquals(100, actual);
     }
     @Test
     public void testRemainAmount0() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(0);
-        assertEquals(actual, 1000);
+        assertEquals(1000, actual);
     }
     @Test
     public void testRemainAmount1() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1);
-        assertEquals(actual, 999);
+        assertEquals(999, actual);
     }
 
     @Test
     public void testRemainAmount999() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(999);
-        assertEquals(actual, 1);
+        assertEquals(1, actual);
     }
 
     @Test
     public void testRemainAmount1000() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
-        assertEquals(actual, 0);
+        assertEquals(0, actual);
 
         //в коде нет проверки, что до текущей границы начисления бонусов больше ничего не нужно докупать
     }
@@ -45,21 +45,21 @@ public class CashbackHackServiceTest {
     public void testRemainAmount1001() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1001);
-        assertEquals(actual, 999);
+        assertEquals(999, actual);
     }
 
     @Test
     public void testRemainAmount1500() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1500);
-        assertEquals(actual, 500);
+        assertEquals(500, actual);
     }
 
     @Test
     public void testRemainAmount2000Negative() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(-2000);
-        assertEquals(actual, 2000);
+        assertEquals(2000, actual);
 
         //нет проверки на ввод отрицательных значений
     }
@@ -68,7 +68,7 @@ public class CashbackHackServiceTest {
     public void testRemainAmount95() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(95);
-        assertEquals(actual, 905);
+        assertEquals(905, actual);
 
     }
 
@@ -76,7 +76,7 @@ public class CashbackHackServiceTest {
     public void testRemainAmount91() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(91);
-        assertEquals(actual, 909);
+        assertEquals(909, actual);
 
     }
 
@@ -84,6 +84,6 @@ public class CashbackHackServiceTest {
     public void testRemainAmount5300() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(5300);
-        assertEquals(actual, 700);
+        assertEquals(700, actual);
     }
 }
